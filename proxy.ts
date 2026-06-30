5fb08f619e2d18d838df5aea3f66c58efda8dc49
Nishchz <anishchz01@gmail.com>
2026-07-01 02:34:27 +0530

    feat: implement credit accumulation logic and secure workspace/projects routes

--- proxy.ts
+++ proxy.ts
@@ -1,20 +1,30 @@
-import { clerkMiddleware, createRouteMatcher } from '@clerk/nextjs/server'
 
-const isProtectedRoute = createRouteMatcher ([
+import { clerkMiddleware, createRouteMatcher } from "@clerk/nextjs/server";
+import { NextResponse } from "next/server";
+
+const isProtectedRoute = createRouteMatcher([
   "/workspace(.*)",
   "/projects(.*)",
 ]);
 
-
-export default clerkMiddleware()
+export default clerkMiddleware(async (auth, req) => {
+  if (isProtectedRoute(req)) {
+    const { userId } = await auth();
+    
+    // Agar userId nahi hai, toh login page par redirect karein
+    if (!userId) {
+      const signInUrl = new URL("/sign-in", req.url);
+      return NextResponse.redirect(signInUrl);
+    }
+  }
+  
+  return NextResponse.next();
+});
 
 export const config = {
   matcher: [
-    // Skip Next.js internals and all static files, unless found in search params
-    '/((?!_next|[^?]*\\.(?:html?|css|js(?!on)|jpe?g|webp|png|gif|svg|ttf|woff2?|ico|csv|docx?|xlsx?|zip|webmanifest)).*)',
-    // Always run for API routes
-    '/(api|trpc)(.*)',
-    // Always run for Clerk-specific frontend API routes
-    '/__clerk/(.*)',
+    "/((?!.\\..|_next).*)",
+    "/",
+    "/(api|trpc)(.*)",
   ],
-}
+};
