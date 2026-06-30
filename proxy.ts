f71e137cd91fb0906ea4b97842ad9bd1c3f0e0c4
Nishchz <anishchz01@gmail.com>
2026-07-01 01:29:54 +0530

    Implement credit accumulation and route protection

--- proxy.ts
+++ proxy.ts
@@ -1,5 +1,9 @@
-import { clerkMiddleware } from '@clerk/nextjs/server'
+import { clerkMiddleware, createRouteMatcher } from '@clerk/nextjs/server'
 
+const isProtectedRoute = createRouteMatcher ([
+  "/workspace(.*)",
+  "/projects(.*)",
+]);
 
 
 export default clerkMiddleware()
